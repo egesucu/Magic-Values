db50547f75688295d6059a920fb9dd7a9c573ba3
Ege Sucu <egesucu@icloud.com>
2023-05-28 15:41:31 +0300

    Initial Commit

---
+++ Magic Values/Magic_ValuesApp.swift
@@ -0,0 +1,17 @@
+//
+//  Magic_ValuesApp.swift
+//  Magic Values
+//
+//  Created by Ege Sucu on 28.05.2023.
+//
+
+import SwiftUI
+
+@main
+struct Magic_ValuesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
